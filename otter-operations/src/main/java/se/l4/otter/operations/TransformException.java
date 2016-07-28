b43f86c027849e03d5d2de6fb1cd2a45c580d76f
Andreas Holstenson <a@holstenson.se>
2016-07-28 10:38:48 +0200

    Adding TransformException

---
+++ otter-operations/src/main/java/se/l4/otter/operations/TransformException.java
@@ -0,0 +1,38 @@
+package se.l4.otter.operations;
+
+/**
+ * Exception for when a combine between two operations fail.
+ * 
+ * @author Andreas Holstenson
+ *
+ */
+public class TransformException
+	extends OperationException
+{
+
+	public TransformException()
+	{
+		super();
+	}
+
+	public TransformException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace)
+	{
+		super(message, cause, enableSuppression, writableStackTrace);
+	}
+
+	public TransformException(String message, Throwable cause)
+	{
+		super(message, cause);
+	}
+
+	public TransformException(String message)
+	{
+		super(message);
+	}
+
+	public TransformException(Throwable cause)
+	{
+		super(cause);
+	}
+	
+}
