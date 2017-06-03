75f892725bf05a94be024c853c1646ee876c85bc
Andreas Holstenson <a@holstenson.se>
2017-06-03 21:21:50 +0200

    Ability to get id of the latest version stored

--- otter-engine/src/main/java/se/l4/otter/engine/DefaultEditorControl.java
+++ otter-engine/src/main/java/se/l4/otter/engine/DefaultEditorControl.java
@@ -17,8 +17,8 @@ import se.l4.otter.operations.OperationPair;
  * consistency. If created without specifying a {@link Lock} it will use a
  * {@link ReentrantLock} and the instance must be reused for all edits to
  * the same document/model. Optionally you can specify your own lock if you
- * for example want to support edits using several servers. 
- * 
+ * for example want to support edits using several servers.
+ *
  * @author Andreas Holstenson
  *
  * @param <Op>
@@ -29,25 +29,25 @@ public class DefaultEditorControl<Op extends Operation<?>>
 	private final OperationHistory<Op> history;
 	private final Lock lock;
 	private final LongIdGenerator idGenerator;
-	
+
 	private final CloseableLock closeableLock;
 
 	public DefaultEditorControl(OperationHistory<Op> history)
 	{
 		this(history, new ReentrantLock());
 	}
-	
+
 	public DefaultEditorControl(OperationHistory<Op> history, Lock lock)
 	{
 		this(history, lock, new SimpleLongIdGenerator());
 	}
-	
+
 	public DefaultEditorControl(OperationHistory<Op> history, Lock lock, LongIdGenerator idGenerator)
 	{
 		this.lock = lock;
 		this.history = history;
 		this.idGenerator = idGenerator;
-		
+
 		closeableLock = new CloseableLock()
 		{
 			@Override
@@ -57,20 +57,20 @@ public class DefaultEditorControl<Op extends Operation<?>>
 			}
 		};
 	}
-	
+
 	@Override
 	public OTType<Op> getType()
 	{
 		return history.getType();
 	}
-	
+
 	@Override
 	public CloseableLock lock()
 	{
 		lock.lock();
 		return closeableLock;
 	}
-	
+
 	@Override
 	public TaggedOperation<Op> getLatest()
 	{
@@ -78,27 +78,33 @@ public class DefaultEditorControl<Op extends Operation<?>>
 		try(CloseableIterator<Op> it = history.until(id + 1))
 		{
 			Composer<Op> composer = history.getType().newComposer();
-			
+
 			while(it.hasNext())
 			{
 				Op op = it.next();
 				composer.add(op);
 			}
-			
+
 			Op composed = composer.done();
-			
+
 			long sessionId = idGenerator.next();
-			
+
 			return new TaggedOperation<>(id, toString(sessionId), composed);
 		}
 	}
-	
+
+	@Override
+	public long getLatestVersion()
+	{
+		return history.getLatest();
+	}
+
 	@Override
 	public TaggedOperation<Op> store(TaggedOperation<Op> operation)
 	{
 		return store(operation.getHistoryId(), operation.getToken(), operation.getOperation());
 	}
-	
+
 	@Override
 	public TaggedOperation<Op> store(long historyBase, String token, Op operation)
 	{
@@ -106,21 +112,21 @@ public class DefaultEditorControl<Op extends Operation<?>>
 		try
 		{
 			long latest = history.getLatest();
-			
+
 			// Get all of the operations that have occurred after our history
 			Op composed;
 			try(CloseableIterator<Op> it = history.from(historyBase + 1))
 			{
 				Composer<Op> composer = history.getType().newComposer();
-				
+
 				while(it.hasNext())
 				{
 					composer.add(it.next());
 				}
-				
+
 				composed = composer.done();
 			}
-			
+
 			// Transform the new operation on top of the composed operation
 			Op transformed;
 			if(composed == null)
@@ -132,7 +138,7 @@ public class DefaultEditorControl<Op extends Operation<?>>
 				OperationPair<Op> pair = history.getType().transform(composed, operation);
 				transformed = pair.getRight();
 			}
-			
+
 			latest = history.store(transformed);
 			return new TaggedOperation<>(latest, token, transformed);
 		}
@@ -141,7 +147,7 @@ public class DefaultEditorControl<Op extends Operation<?>>
 			lock.unlock();
 		}
 	}
-	
+
 	private final static char[] DIGITS = {
 		'0', '1', '2', '3', '4', '5',
 		'6', '7', '8', '9', 'a', 'b',
@@ -155,9 +161,9 @@ public class DefaultEditorControl<Op extends Operation<?>>
 		'S', 'T', 'U', 'V', 'W', 'X',
 		'Y', 'Z'
     };
-	
+
 	private final static int MAX = DIGITS.length;
-	
+
 	public static String toString(long i)
 	{
 		char[] buf = new char[11];
--- otter-engine/src/main/java/se/l4/otter/engine/EditorControl.java
+++ otter-engine/src/main/java/se/l4/otter/engine/EditorControl.java
@@ -6,7 +6,7 @@ import se.l4.otter.operations.Operation;
 
 /**
  * Helper for keeping several editors in sync.
- * 
+ *
  * @author Andreas Holstenson
  *
  */
@@ -14,50 +14,57 @@ public interface EditorControl<Op extends Operation<?>>
 {
 	/**
 	 * Get the type this control handles.
-	 * 
+	 *
 	 * @return
 	 */
 	OTType<Op> getType();
-	
+
 	/**
 	 * Lock this instance, this ensures that no other operation occur
 	 * on the editor control while the lock is being held. This is useful
 	 * to guarantee sending of changes in order similar to this:
-	 * 
+	 *
 	 * <pre>
 	 * try(CloseableLock lock = control.lock()) {
 	 *   TaggedOperation change = control.store(taggedOp);
 	 *   remoteClients.send(change);
 	 * }
 	 * </pre>
-	 * 
+	 *
 	 * @return
 	 */
 	CloseableLock lock();
-	
+
 	/**
 	 * Get the operation that describes the document that a new client should
 	 * start with.
-	 * 
+	 *
 	 * @return
 	 */
 	TaggedOperation<Op> getLatest();
-	
+
+	/**
+	 * Get the identifier of the latest stored operation.
+	 *
+	 * @return
+	 */
+	long getLatestVersion();
+
 	/**
 	 * Store a new operation using {@link TaggedOperation#getHistoryId()} as
 	 * the history base. This method returns a transformed operation that
 	 * other editors should apply.
-	 * 
+	 *
 	 * @param operation
 	 * @return
 	 */
 	TaggedOperation<Op> store(TaggedOperation<Op> operation);
-	
+
 	/**
 	 * Store a new operation using the specified history identifier as a base.
 	 * This method returns a transformed operation that other editors should
 	 * apply.
-	 * 
+	 *
 	 * @param historyBase
 	 *   the history identifier the operation applies to
 	 * @param token
