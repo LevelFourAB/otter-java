d51765aae41d8a64a6b742c5126eae3121ca9b82
Andreas Holstenson <a@holstenson.se>
2017-06-03 21:21:15 +0200

    Improving algorithm used by DefaultComposer
    Instead of doing linear merges it now merges in a tree-like manner just
    like Apache Wave.

--- otter-operations/src/main/java/se/l4/otter/operations/internal/DefaultComposer.java
+++ otter-operations/src/main/java/se/l4/otter/operations/internal/DefaultComposer.java
@@ -1,12 +1,16 @@
 package se.l4.otter.operations.internal;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import se.l4.otter.operations.Composer;
 import se.l4.otter.operations.OTType;
 import se.l4.otter.operations.Operation;
 
 /**
- * Default implementation of {@link Composer}.
- * 
+ * Default implementation of {@link Composer}. Based on the same algorithm
+ * that Apache Wave uses to merge composed operations in a tree like manner.
+ *
  * @author Andreas Holstenson
  *
  * @param <Op>
@@ -15,30 +19,61 @@ public class DefaultComposer<Op extends Operation<?>>
 	implements Composer<Op>
 {
 	private final OTType<Op> type;
-	private Op result;
+
+	private final List<Op> operations;
 
 	public DefaultComposer(OTType<Op> type)
 	{
 		this.type = type;
+
+		operations = new ArrayList<>();
 	}
-	
+
 	@Override
 	public Composer<Op> add(Op op)
 	{
-		if(result == null)
+		if(operations.isEmpty())
 		{
-			result = op;
+			operations.add(op);
 		}
 		else
 		{
-			result = type.compose(result, op);
+			for(int i=0, n=operations.size(); i<n; i++)
+			{
+				Op nextOp = operations.get(i);
+				if(nextOp == null)
+				{
+					operations.set(i, op);
+					return this;
+				}
+
+				operations.set(i, null);
+				op = type.compose(nextOp, op);
+			}
+			operations.add(op);
 		}
+
 		return this;
 	}
-	
+
 	@Override
 	public Op done()
 	{
+		Op result = null;
+		for(Op op : operations)
+		{
+			if(op != null)
+			{
+				if(result == null)
+				{
+					result = op;
+				}
+				else
+				{
+					result = type.compose(op, result);
+				}
+			}
+		}
 		return result;
 	}
 }
